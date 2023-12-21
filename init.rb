872d0fad087a7f26fdc52f6141db804b1767c722
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2023-12-21 11:59:55 -0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
-Redmine::Plugin.register :cti_plugin do
-  name 'CTI Plugin MMulheres'
+Redmine::Plugin.register :cti_plugin_senev do
+  name 'CTI Plugin SENEV/MMulheres'
   author 'CTI Equipe'
-  description 'Este é um plugin do Redmine, feito pela equipe da CTI do Ministério das Mulheres'
+  description 'Este é um plugin do Redmine, feito pela equipe da CTI do Ministério das Mulheres, para o sistema de gestão SENEV'
   version '0.0.1'
-  url 'https://github.com/clelsonsalles/cti_plugin/'
+  url 'https://github.com/clelsonsalles/cti_plugin_senev/'
   author_url 'http://example.com/about'
 
   settings :default => {
